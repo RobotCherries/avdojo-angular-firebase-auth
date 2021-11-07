7aaa5ad6fef7170e742ad8bb6f5c740c6118c345
RobotCherries <zdip14@gmail.com>
2021-11-07 23:41:45 +0200

    Added angular cdk & browser animations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,10 +15,19 @@ import { LoginComponent } from './login/login.component';
     LoginComponent,
   ],
   imports: [
+    // Angular
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    // Firebase
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    FontAwesomeModule
+
+    // UI Libraries
+    NgbModule,
+
+    // App
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
